3a6e4fca5964c6a6f259887fd5c694e154ca4267
challasaicharanreddy <challasaicharan3@gmail.com>
2026-09-02 02:29:15 +0530

    resolving conflicts in transactions.service.js
    Refactor transaction service to include portfolio retrieval and add getRecentTransactions function.

--- backend/src/services/transactions.service.js
+++ backend/src/services/transactions.service.js
@@ -1,6 +1,6 @@
 import transactions from "../models/transactions.js";
-
-export default async function(userid) {
+import Portfolio from "../models/portfolio.js";
+export async function transaction(userid) {
     const data=await transactions.find({user:userid}).sort({executedAt:-1});
     let buyorders=0;
     let sellorders=0;
@@ -20,4 +20,30 @@ export default async function(userid) {
         avgtradesize:data.length>0?(totalvolume/data.length):0,
         record:data
     }
+}
+export async function getRecentTransactions(userId) {
+
+    const portfolio = await Portfolio.findOne({
+        user: userId
+    });
+
+    if (!portfolio) {
+        throw new Error("Portfolio not found");
+    }
+
+    const transaction = await transactions.find({
+        portfolio: portfolio._id
+    })
+        .sort({ executedAt: -1 })
+        .limit(5)
+        .lean();
+
+    return transaction.map((transactionn) => ({
+        id: transactionn._id,
+        action: transactionn.action,
+        symbol: transactionn.symbol,
+        quantity: transactionn.quantity,
+        price: transactionn.price,
+        executedAt: transactionn.executedAt
+    }));
 }
