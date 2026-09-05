80ca897815ad4602b12bb517109353e700217afc
Abhiram2406 <abhiramnalla2406@gmail.com>
2026-09-05 17:44:15 +0530

    Update fetchStockMinutes.js deployment bug fix-3

--- backend/src/services/fetchStockMinutes.js
+++ backend/src/services/fetchStockMinutes.js
@@ -1,4 +1,4 @@
-import MinuteCandles from "../models/MinuteCandles.js";
+import MinuteCandles from "../models/minuteCandles.js";
 const Model=MinuteCandles
 export default async function(symbol,timeperiod) {
     const time=24*60*60*1000;
